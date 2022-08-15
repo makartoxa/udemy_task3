3fdfa936c8d3ebb57c94ef79022ed3bd7789a559
user2507 <makar.toxa@gmail.com>
2022-08-15 13:31:59 +0300

    add operator ?.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
 'use strict';
 
 const box = document.querySelector('.box');
+const block = document.querySelector('.block');
 
-const newHeight = 100;
-const newWidth = 400;
+// console.log(block);
 
-function changeParams(elem, h, w) {
-    elem.style.height = `${h ?? 200}px`;
-    elem.style.width = `${w ?? 200}px`;
-    elem.innerHTML = (h ?? 200) * (w ?? 200);
+// if(block) {
+//     console.log(block.textContent);
+// };
+
+// block?.textContent = '123';
+//
+// console.log(1 + 2);
+
+const userData = {
+    name: 'Ivan',
+    age: null,
+    say: function () {
+        console.log('Hello');
+    }
 }
+userData.say();
+userData.hey?.();
+// if (userData && userData.skills && userData.skills.js) {
+//     console.log(userData.skills.js);
+// }
 
-changeParams(box, newHeight, newWidth);
+console.log(userData.skills?.js);
